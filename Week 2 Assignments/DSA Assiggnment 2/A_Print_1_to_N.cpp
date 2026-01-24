2285477240c60d59bb6fbde05dec396b88299e3a
Bibek-M <bibek7020u@gmail.com>
2026-01-24 21:37:51 +0530

    A,B,C,D,E

---
+++ Week 2 Assignments/DSA Assiggnment 2/A_Print_1_to_N.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter a number"<<endl;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        cout<<i+1<<endl;
+    }
+}
---
+++ Week 2 Assignments/DSA Assiggnment 2/D_Print_from_L_to_R.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int l;
+    int r;
+    cout << "Enter a L" << endl;
+    cin >> l;
+    cout << "Enter a R" << endl;
+    cin>> r;
+    for (int i = l; i <= r; i++)
+    {
+        cout << i <<" ";
+    }
+}
